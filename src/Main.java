ff18fcefc0c67f796aae4fc8d68653b30b878183
ShunmugaSundaram <shunmugasundaram2021plan@gmail.com>
2024-02-19 09:01:04 +0530

    my 1st commit on 19th feb 2024

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+
+        for (int i = 1; i <= 7; i++) {
+            System.out.println("i = " + i);
+        }
+        System.out.println();
+        for (int j = 1; j <= 10; j++) {
+            System.out.println("j = " + j);
+        }
+    }
+}
